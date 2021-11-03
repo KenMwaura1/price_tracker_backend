6d3e9088ddac58332b9772d8ea371a403af17338
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-03 15:00:52 +0300

    updated pipelines

--- price_scraper/price_scraper/settings.py
+++ price_scraper/price_scraper/settings.py
@@ -72,9 +72,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'price_scraper.pipelines.PriceScraperPipeline': 300,
-# }
+ITEM_PIPELINES = {
+    'price_scraper.pipelines.PriceScraperPipeline': 300,
+ }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
