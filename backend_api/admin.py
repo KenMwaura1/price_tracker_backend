ef2a0677719ddcde6672deb5de314a0259776b91
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-11-04 22:45:12 +0300

    add admin.py

--- backend_api/admin.py
+++ backend_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Product
+from .models import Profile
 # Register your models here.
+
+
+admin.site.register(Profile)
+admin.site.register(Product)
