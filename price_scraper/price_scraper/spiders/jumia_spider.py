f12a4ee498de02574996a4760a182e59d3ff37ba
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-04 23:04:28 +0300

    extraction logic

--- price_scraper/price_scraper/spiders/jumia_spider.py
+++ price_scraper/price_scraper/spiders/jumia_spider.py
@@ -6,5 +6,13 @@ class JumiaSpiderSpider(scrapy.Spider):
     allowed_domains = ['jumia.co.ke']
     start_urls = ['http://jumia.co.ke/']
 
+    def start_requests(self):
+        urls = [
+            'https://www.jumia.co.ke/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
     def parse(self, response):
+
         pass
