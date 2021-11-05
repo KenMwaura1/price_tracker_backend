774e6426ca4970bf867af9a27cdd6642ba5f7a89
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-05 04:16:28 +0300

    class to handle items

--- price_scraper/price_scraper/pipelines.py
+++ price_scraper/price_scraper/pipelines.py
@@ -6,8 +6,15 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
 class PriceScraperPipeline:
     def process_item(self, item, spider):
-        return item
+        adapter = ItemAdapter(item)
+        if adapter.get('current_price'):
+            adapter.update({'current_price': adapter.get('current_price').replace('$', '')})
+            item.save()
+            return item
+        else:
+            raise DropItem("Missing current_price in %s" % item)
