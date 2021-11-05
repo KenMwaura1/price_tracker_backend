3959d2dc9d941e08c2a8a58e5c5772aadfc56da1
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-05 06:00:25 +0300

    fixed error of having duplicate items in the output

--- price_scraper/price_scraper/pipelines.py
+++ price_scraper/price_scraper/pipelines.py
@@ -10,11 +10,16 @@ from scrapy.exceptions import DropItem
 
 
 class PriceScraperPipeline:
+    def __init__(self):
+        self.names_seen = set()
+
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
-        if adapter.get('current_price'):
-            adapter.update({'current_price': adapter.get('current_price').replace('$', '')})
-            item.save()
-            return item
-        else:
+        if not adapter.get('current_price'):
             raise DropItem("Missing current_price in %s" % item)
+        # adapter.update({'current_price': adapter.get('current_price').replace('$', '')})
+        if adapter.get('name') in self.names_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        self.names_seen.add(adapter.get('name'))
+        item.save()
+        return item
