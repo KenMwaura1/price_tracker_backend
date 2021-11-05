63b806a17cd9fab22f411d3bd1db41c5f2c38ce6
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-05 08:14:53 +0300

    GitHub merge fixes

--- backend_api/views.py
+++ backend_api/views.py
@@ -255,7 +255,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created..!')
+            messages.success(request, 'Your account has been created..!')
             return redirect('login')
     else:
 
